0490ea2a13f775010c4d5bbd8736a0439ba8bea5
Alice Bianchi Nogueira <alicebianchi2013@gmail.com>
2023-08-27 01:54:41 -0300

    Edição e exclusão de itens na aula do dia 25-08

--- Android Kotlin Developer/[a-2-1] RecyclerView/app/src/main/java/com/example/recyclerview/IDialogItem.kt
+++ Android Kotlin Developer/[a-2-1] RecyclerView/app/src/main/java/com/example/recyclerview/IDialogItem.kt
@@ -2,4 +2,7 @@ package com.example.recyclerview
 
 interface IDialogItem {
     fun addItem(item: ItemModel)
+    fun openEditItem(posicao:Int, item: ItemModel)
+    fun editItem(posicao: Int, item: ItemModel)
+    fun removeItem(item: ItemModel)
 }
--- Android Kotlin Developer/[a-2-1] RecyclerView/app/src/main/java/com/example/recyclerview/PopupItemDialog.kt
+++ Android Kotlin Developer/[a-2-1] RecyclerView/app/src/main/java/com/example/recyclerview/PopupItemDialog.kt
@@ -10,21 +10,36 @@ class PopupItemDialog : DialogFragment() {
 
     lateinit var bind: PopupItemDialogBinding
     var iDialogInference: IDialogItem? = null
+    private var itemPosition: Int = -1
+    private var editedItem: ItemModel? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         //return super.onCreateDialog(savedInstanceState)
         bind = PopupItemDialogBinding.inflate(layoutInflater)
 
         val builder = AlertDialog.Builder(requireContext())
+
+        editedItem?.let { item ->
+            bind.textView2.text = "Edição"
+            bind.txtTitle.setText(item.item)
+            bind.txtDescription.setText(item.detalhe)
+        }
+
         builder.setView(bind.root)
             .setPositiveButton("Confirmar", { dialog, _ ->
-                iDialogInference?.addItem(
-                    ItemModel(
-                        bind.txtTitle.text.toString(),
-                        bind.txtDescription.text.toString(),
-                        false
-                    )
+
+                val popupItem = ItemModel(
+                    bind.txtTitle.text.toString(),
+                    bind.txtDescription.text.toString(),
+                    false
                 )
+                if (itemPosition > -1) {
+                    iDialogInference?.editItem(itemPosition, popupItem)
+                } else {
+                    iDialogInference?.addItem(
+                        popupItem
+                    )
+                }
                 dialog.cancel()
             })
             .setNegativeButton("Cancelar"){dialog, _ ->
@@ -35,10 +50,16 @@ class PopupItemDialog : DialogFragment() {
     }
 
     companion object {
-        fun buildDialog(iDialog: IDialogItem):PopupItemDialog {
+        fun buildDialog(
+            iDialog: IDialogItem,
+            posicao: Int = -1,
+            item: ItemModel? = null
+        ):PopupItemDialog {
             val dialog = PopupItemDialog()
             val args = Bundle()
             dialog.iDialogInference = iDialog
+            dialog.editedItem = item
+            dialog.itemPosition = posicao
             dialog.arguments = args
             return dialog
         }
