2b416bbae6a5b2f03604ed3d8d4fd687c788c451
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 22:35:34 -0300

    ApiDeckGame

---
+++ Android Kotlin Developer/ApiDeckGame/app/src/main/java/com/example/apideckgame/viewModel/DeckViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.example.apideckgame.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.apideckgame.NetworkUtils
+import retrofit2.Retrofit
+
+class DeckViewModelFactory(
+    private val retrofit: Retrofit = NetworkUtils.getRetrofitInstance("https://deckofcardsapi.com/api/deck/")
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return with(modelClass){
+            when{
+                isAssignableFrom(DeckViewModel::class.java) ->
+                    DeckViewModel(retrofit)
+                else ->
+                    throw IllegalStateException("classe desconhecida")
+            }
+        } as T
+    }
+
+}
